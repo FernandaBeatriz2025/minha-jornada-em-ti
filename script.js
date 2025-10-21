44a6d87c8d36eaf873fe367d1de855b0b68e059b
Fernanda <fernanda.spoby@gmail.com>
2025-10-21 13:01:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Por enquanto, o site não tem interações complexas
+// Mas aqui você pode adicionar futuras animações ou funções JS
+
+console.log("Minha Jornada em TI - Site carregado com sucesso!");
